cc54183910e21bfe49657c6c14b8d158329148a9
jinbo-0523 <65935770+jinbo-0523@users.noreply.github.com>
2021-04-20 23:17:15 +0900

    let変数

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 // 変数宣言
 // const let
-var val1 = "var変数";
-console.log(val1);
+// var val1 = "var変数";
+// console.log(val1);
 
-var val1 = "var変数を上書き";
-console.log(val1);
+// var val1 = "var変数を上書き";
+// console.log(val1);
 
-// varは再宣言できる
+// // varは再宣言できる
 
-var val1 = "var変数を再宣言";
-console.log(val1);
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+let val2 = "let変数";
+console.log(val2);
+
+// letは上書き可能
+
+val2 = "let変数を上書き";
+console.log(val2);
+
+// letは再宣言不可能
+let val2 = "let変数を再宣言";
