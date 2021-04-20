d4ad48b834d9806cb10e4fa7f3e3881a1865a0af
jinbo-0523 <65935770+jinbo-0523@users.noreply.github.com>
2021-04-20 23:05:59 +0900

    var変数について

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import "./styles.css";
+// 変数宣言
+// const let
+var val1 = "var変数";
+console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var val1 = "var変数を上書き";
+console.log(val1);
+
+// varは再宣言できる
+
+var val1 = "var変数を再宣言";
+console.log(val1);
