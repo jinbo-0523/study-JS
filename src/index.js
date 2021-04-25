c60534212898a51701dd8df4b0e4311f83f32dad
jinbo-0523 <65935770+jinbo-0523@users.noreply.github.com>
2021-04-25 18:27:47 +0900

    &&と||について

--- src/index.js
+++ src/index.js
@@ -199,19 +199,41 @@
 // const val1 = 1>0 ? "trueです"　:　"falseです";
 // console.log(val1);
 
-const num = "1300";
-console.log(num.toLocaleString());
-// toLocaleString とは1,300のように３
-// 桁区切りにカンマがを入れられる関数。
-// 文字列には反応しないので三項演算子を使って
-// 数値の場合は３桁区切り、文字列の場合には「文字列を入力してください」と表示せるようにする
-const formattedNum =
-  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
-// typeofとは型を判別してくれる
-console.log(formattedNum);
+// const num = "1300";
+// console.log(num.toLocaleString());
+// // toLocaleString とは1,300のように３
+// // 桁区切りにカンマがを入れられる関数。
+// // 文字列には反応しないので三項演算子を使って
+// // 数値の場合は３桁区切り、文字列の場合には「文字列を入力してください」と表示せるようにする
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+// // typeofとは型を判別してくれる
+// console.log(formattedNum);
 
-// 値の合計が100以下かどうかをチェックする関数checkSumを作成するとする
-const checkSum = (num1, num2) => {
-  return num1 + num2 > 100 ? "１００を超えています" : "許容範囲内です";
-};
-console.log(checkSum(50, 60));
+// // 値の合計が100以下かどうかをチェックする関数checkSumを作成するとする
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "１００を超えています" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 60));
+
+// 論理演算子の本当の意味を知ろう　&& ||
+// const flag1 = true;
+// const flag2 = false;
+// 「または」とか「かつ」で読みがち
+// if(flag1 || flag2){
+//   console.log("1か2はtrueになります");
+// }
+// if(flag1 && flag2){
+//   console.log("1も2はtrueになります");
+// }
+// ||
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee);
+// 実際には、||の左側がfalseの時、右を返す
+// nullはflase判定になるため||の右側『金額未設です』が返される。
+
+// &&　は左側がtrueなら右を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
